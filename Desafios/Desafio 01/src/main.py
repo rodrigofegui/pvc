9ea4a82a91a4685f3267e71cf18462b7d1de7882
Rodrigo Ferrerira Guimarães <rodrigofegui@gmail.com>
2021-02-09 21:35:49 -0300

    Add: VirtualBackground com detecçao de face pura

--- Desafios/Desafio 01/src/main.py
+++ Desafios/Desafio 01/src/main.py
@@ -1,21 +1,34 @@
 import cv2
 import numpy as np
-from utils import get_background_image
-from virtual_background import background_subtraction
+from utils import stackImages, get_background_image
+from virtual_background import background_subtraction, face_detection
 
 
-def main():
+def main(processing_method):
     webcam = cv2.VideoCapture(0)
 
     background_img = get_background_image(webcam)
 
-    background_subtraction(webcam, background_img)
+    _, fst_frame = webcam.read()
+
+    while True:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        _, cur_frame = webcam.read()
+
+        mask = processing_method(cur_frame, fst_frame)
+
+        masked_frame = cv2.bitwise_and(cur_frame, cur_frame, mask=mask)
+        indexes = np.where(masked_frame == 0)
+        masked_frame[indexes] = background_img[indexes]
+
+        stacked_img = stackImages(.5, ([cur_frame, background_img, masked_frame]))
+        cv2.imshow("Imagens", stacked_img)
 
     webcam.release()
     cv2.destroyAllWindows()
 
-def empty(val):
-    pass
 
 if __name__ == '__main__':
-    main()
+    main(face_detection)
--- Desafios/Desafio 01/src/virtual_background.py
+++ Desafios/Desafio 01/src/virtual_background.py
@@ -1,38 +1,40 @@
 import cv2
 import numpy as np
-from utils import stackImages
 
 
-def background_subtraction(webcam, background_img):
-    _, key_frame = webcam.read()
-    key_frame = cv2.cvtColor(key_frame, cv2.COLOR_BGR2GRAY)
+def background_subtraction(cur_frame, fst_frame):
+    fst_frame = cv2.cvtColor(fst_frame, cv2.COLOR_BGR2GRAY)
 
-    while True:
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    img_gray = cv2.cvtColor(cur_frame, cv2.COLOR_BGR2GRAY)
 
-        _, img = webcam.read()
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    diff_img = cv2.subtract(img_gray, fst_frame)
+    diff_mean = np.mean(diff_img)
 
-        diff_img = cv2.subtract(img_gray, key_frame)
-        diff_mean = np.mean(diff_img)
+    mask_weight = 1
+    if diff_mean <= 70:
+        mask_weight = .5
+    elif diff_mean <= 85:
+        mask_weight = 1.1
+    elif diff_mean <= 170:
+        mask_weight = .75
 
-        mask_weight = 1
-        if diff_mean <= 70:
-            mask_weight = .5
-        elif diff_mean <= 85:
-            mask_weight = 1.1
-        elif diff_mean <= 170:
-            mask_weight = .75
+    _, mask = cv2.threshold(diff_img, int(diff_mean * mask_weight), 255, cv2.THRESH_BINARY_INV)
+    mask = cv2.erode(mask, np.ones((3,3)), iterations=2)
+    mask = cv2.dilate(mask, np.ones((3,3)), iterations=3)
+    mask = cv2.erode(mask, np.ones((3,3)), iterations=1)
 
-        _, mask = cv2.threshold(diff_img, int(diff_mean * mask_weight), 255, cv2.THRESH_BINARY_INV)
-        mask = cv2.erode(mask, np.ones((3,3)), iterations=2)
-        mask = cv2.dilate(mask, np.ones((3,3)), iterations=3)
-        mask = cv2.erode(mask, np.ones((3,3)), iterations=1)
+    return mask
 
-        masked_img = cv2.bitwise_and(img, img, mask=mask)
-        indexes = np.where(masked_img == 0)
-        masked_img[indexes] = background_img[indexes]
+def face_detection(cur_frame, _):
+    faceCascade= cv2.CascadeClassifier('resources/haarcascade_frontalface_default.xml')
 
-        stacked_img = stackImages(.5, ([img, background_img, masked_img]))
-        cv2.imshow("Imagens", stacked_img)
+    gray_frame = cv2.cvtColor(cur_frame, cv2.COLOR_BGR2GRAY)
+    mask = np.zeros_like(gray_frame)
+
+    faces = faceCascade.detectMultiScale(gray_frame, 1.1, 4)
+
+    for x, y, w, h in faces:
+        cv2.rectangle(mask, (x, y), (x + w, y + h), (255, 0, 0), -1)
+
+    return mask
+
