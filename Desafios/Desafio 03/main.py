199e657f2fa769c5ebf6177c8f1f159f4ff908a4
Rodrigo Ferrerira Guimarães <rodrigofegui@gmail.com>
2021-03-17 22:19:33 -0300

    Add: Desafio 3 funcionando

--- Desafios/Desafio 03/main.py
+++ Desafios/Desafio 03/main.py
@@ -3,7 +3,7 @@ from src.camera_handler import get_camera_matrices, get_real_and_img_pts
 from src.utils import draw_cursor, draw_clicks, calculate
 
 CHESS_GRID_SHAPE = (7, 7)
-REAL_CHESS_SQ_SIZE = 2.1
+REAL_CHESS_SQ_SIZE = 1
 IMG_CHESS_SRC_PATTERN = './images/*.jpg'
 SHOW_CHESS_ANALYSES = False
 WEBCAM_WIN_NAME = 'Webcam'
@@ -16,12 +16,22 @@ real_pts, img_pts, img_shape, img_names = get_real_and_img_pts(
 matrices = get_camera_matrices(real_pts, img_pts, img_shape)
 obj_pts = []
 cursor_lines = []
+can_calculate = False
 
 def mouse_handler(event, x, y, flags, param):
-    global cur_frame, obj_pts, cursor_lines
+    global cur_frame, obj_pts, cursor_lines, can_calculate
 
     if event == cv.EVENT_LBUTTONDOWN:
-        obj_pts.append((x, y))
+        pt = (x, y)
+
+        if pt not in obj_pts:
+            obj_pts.append(pt)
+
+        if len(obj_pts) >= 2:
+            can_calculate = True
+
+            if len(obj_pts) > 2:
+                obj_pts.pop(0)
     elif event == cv.EVENT_MOUSEMOVE:
         cursor_lines = [
             ((x, 0), (x, cur_frame.shape[0])),
@@ -29,11 +39,15 @@ def mouse_handler(event, x, y, flags, param):
         ]
 
 def distance_handler(a):
-    print('distance_handler', a)
+    global can_calculate
+
+    can_calculate = True
     cv.setTrackbarPos('Size (cm)', INPUT_WIN_NAME, 0)
 
 def size_handler(s):
-    print('size_handler', s)
+    global can_calculate
+
+    can_calculate = True
     cv.setTrackbarPos('Distance (cm)', INPUT_WIN_NAME, 0)
 
 webcam = cv.VideoCapture(0)
@@ -42,8 +56,8 @@ cv.setMouseCallback(WEBCAM_WIN_NAME, mouse_handler)
 
 cv.namedWindow(INPUT_WIN_NAME)
 
-cv.createTrackbar('Distance (cm)', INPUT_WIN_NAME, 0, 200, distance_handler)
-cv.createTrackbar('Size (cm)', INPUT_WIN_NAME, 0, 100, size_handler)
+cv.createTrackbar('Distance (cm)', INPUT_WIN_NAME, 0, 80, distance_handler)
+cv.createTrackbar('Size (cm)', INPUT_WIN_NAME, 0, 30, size_handler)
 
 resp = ''
 while True:
@@ -54,24 +68,27 @@ while True:
     elif pressed_key == ord('r'):
         obj_pts.clear()
         resp = ''
-    elif pressed_key == ord('\r'):
-        resp = calculate(
-            matrices,
-            obj_pts,
-            cv.getTrackbarPos('Distance (cm)', INPUT_WIN_NAME),
-            cv.getTrackbarPos('Size (cm)', INPUT_WIN_NAME)
-        )
 
     _, cur_frame = webcam.read()
 
     cur_frame = draw_cursor(cur_frame, cursor_lines)
     cur_frame = draw_clicks(cur_frame, obj_pts)
 
-    cv.rectangle(cur_frame, (0, 0), (cur_frame.shape[1], 30), (38, 0, 77), cv.FILLED)
-    cv.putText(cur_frame, 'q: quit | r: reset points | ENTER: calculate', (70, 20), FONT, .7, (255, 249, 115), 1, cv.LINE_AA)
+    cv.rectangle(cur_frame, (0, 0), (cur_frame.shape[1], 30), (77, 0, 38), cv.FILLED)
+    cv.putText(cur_frame, 'q: quit | r: reset points', ((cur_frame.shape[0] // 2) - (5 * len('q: quit | r: reset points') // 2), 20), FONT, .7, (115, 249, 255), 1, cv.LINE_AA)
+
+    if can_calculate:
+        resp = calculate(
+            matrices,
+            obj_pts,
+            cv.getTrackbarPos('Distance (cm)', INPUT_WIN_NAME),
+            cv.getTrackbarPos('Size (cm)', INPUT_WIN_NAME)
+        )
+        can_calculate = False
 
     if resp:
-        cv.putText(cur_frame, resp, (5, cur_frame.shape[0] - 20), FONT, .7, (255, 249, 115), 1, cv.LINE_AA)
+        cv.rectangle(cur_frame, (0, cur_frame.shape[0] - 50), (200, cur_frame.shape[0]), (77, 0, 38), cv.FILLED)
+        cv.putText(cur_frame, resp, (5, cur_frame.shape[0] - 20), FONT, .7, (115, 249, 255), 1, cv.LINE_AA)
 
     cv.imshow(WEBCAM_WIN_NAME, cur_frame)
 
--- Desafios/Desafio 03/src/utils.py
+++ Desafios/Desafio 03/src/utils.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 
+
 def draw_cursor(image, cursor_lines):
     for start, end in cursor_lines:
         cv.line(image, start, end, (0, 0, 255), 1)
@@ -14,4 +15,26 @@ def draw_clicks(image, points):
     return image
 
 def calculate(matrices, points, dist, size):
-    return 'medida: x.xx xx'
+    if not ((dist or size) and len(points) == 2):
+        return ''
+
+    pt1 = np.array([[*points[0], 1]]).astype(float)
+    pt2 = np.array([[*points[1], 1]]).astype(float)
+
+    omega = np.linalg.inv(np.matmul(matrices['intrinsics'], matrices['intrinsics'].T))
+
+    numerator = np.matmul(np.matmul(pt1, omega), pt2.T)
+    denominator1 = np.sqrt(np.matmul(np.matmul(pt1, omega), pt1.T))
+    denominator2 = np.sqrt(np.matmul(np.matmul(pt2, omega), pt2.T))
+
+    cos_theta = (numerator / (denominator1 * denominator2))[0][0]
+    theta = np.arccos(cos_theta)
+
+    if dist:
+        quantity = 'size'
+        measure = (dist * np.tan(theta / 2)) / 2
+    elif size:
+        quantity = 'dist'
+        measure = size / (2 * np.tan(theta / 2))
+
+    return f'{quantity}: {measure:.2f} cm'
