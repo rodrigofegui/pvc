005ce98eec8cfc36f0ecdb8e0642816c1b76cb2a
Rodrigo Ferrerira Guimarães <rodrigofegui@gmail.com>
2021-03-21 21:47:56 -0300

    Add: Mapa de disparidade ajustando janela de busca

--- Projetos/Projeto_1/src/utils/utils.py
+++ Projetos/Projeto_1/src/utils/utils.py
@@ -35,8 +35,8 @@ def get_resize_shape(original_shape: tuple) -> tuple:
     return IMG_LOWEST_DIMENSION, int((IMG_LOWEST_DIMENSION * original_shape[0]) / original_shape[1])
 
 def get_disp_map(img_left:np.ndarray, img_right:np.ndarray, use_default: bool = False) -> np.ndarray:
-    img_left = cv.cvtColor(img_left, cv.COLOR_BGR2GRAY)
-    img_right = cv.cvtColor(img_right, cv.COLOR_BGR2GRAY)
+    img_left = cv.cvtColor(img_left, cv.COLOR_BGR2GRAY).astype(np.int32)
+    img_right = cv.cvtColor(img_right, cv.COLOR_BGR2GRAY).astype(np.int32)
 
     if use_default:
         return cv.StereoBM_create(
@@ -44,49 +44,63 @@ def get_disp_map(img_left:np.ndarray, img_right:np.ndarray, use_default: bool = 
             blockSize=5
         ).compute(img_left, img_right)
 
-    filter_sz = int(max(MIN_DISP_FILTER_SZ, min(*img_left.shape) / DISP_FILTER_RATIO))
-    padding = filter_sz // 2
-    padding += 0 if padding % 2 == 0 else 1
+    # filter_sz = int(max(MIN_DISP_FILTER_SZ, min(*img_left.shape) / DISP_FILTER_RATIO))
+    # padding = filter_sz // 2
+    # padding += 0 if padding % 2 == 0 else 1
+    filter_sz = None
     padding = 1
     print('filter', filter_sz, padding)
 
     disp_map = np.zeros_like(img_left)
+    matched = np.zeros_like(img_left)
 
-    img_left = np.pad(
-        img_left, [(padding, padding), (padding, padding)], mode='constant', constant_values=0
-    ).astype(np.int32)
-    img_right = np.pad(
-        img_right, [(padding, padding), (padding, padding)], mode='constant', constant_values=0
-    ).astype(np.int32)
-
-    for unpadding_y, y in enumerate(range(padding, img_left.shape[0] - padding)):
+    for y in range(img_left.shape[0]):
     # for unpadding_y, y in enumerate(range(50, img_left.shape[0] - padding)):
         print(f'linha: {y}')
-        for unpadding_x, x in enumerate(range(padding, img_left.shape[1] - padding)):
-            match_sum, match_x = float('inf'), x
-            ref = img_left[y - padding:y + padding + 1, x - padding:x + padding + 1]
+        for x_l in range(img_left.shape[1] - 1, -1, -1):
+            # print(f'coluna: {x_l}')
+            sum_match, x_match = float('inf'), x_l
             # print(f'ref[{y}, {x}]:\n{ref}')
 
-            for xs in range(x, max(padding, x - DISP_BLOCK_SEARCH) - 1, x + 1):
-                search = img_right[y - padding:y + padding + 1, xs - padding:xs + padding + 1]
-                diff = abs(ref - search)
+            for x_r in range(np.argmax(matched[y]) or x_l, -1, -1):
+                x_min, y_min = max(0, x_r - padding), max(0, y - padding)
+                x_max, y_max = min(x_r + padding + 1, img_left.shape[1]), min(y + padding + 1, img_left.shape[0])
+
+                # Shape match ajustment
+                x_l_max = min(x_l + x_max - x_r, img_left.shape[1])
+                x_max = min(x_r + x_l_max - x_l, img_left.shape[1])
+
+                search = img_right[y_min:y_max, x_min:x_max]
+                ref = img_left[y_min:y_max, x_l + x_min - x_r:x_l_max]
+                try:
+                    diff = abs(ref - search)
+                    cmp_sum = np.sum(diff)
+                # print(f'\ndiff: {cmp_sum} {diff.shape}\n{diff}')
                 # print(f'search[{y}, {x}]:\n{search}')
                 # print(f'diff: {np.sum(diff)} || {match_sum}')
-                cmp_sum = np.sum(diff)
+                except:
+                    print(f'\nRIGHT [{y}, {x_r}]: x_min: {x_min} | x_max: {x_max} | y_min: {y_min} | y_max: {y_max}')
+                    print(f'LEFT [{y}, {x_l}]: x_min: {x_l + x_min - x_r} | x_max: {x_l_max} | y_min: {y_min} | y_max: {y_max}')
+                    print(f'ref[{y}, {x_l}]: {ref.shape}\n{ref}')
+                    print(f'search[{y}, {x_r}]: {search.shape}\n{search}')
 
-                if cmp_sum < match_sum:
-                    # print(f'\t{x} -> {xs}')
-                    match_sum = cmp_sum
-                    match_x = xs
+                    print(1/0)
+
+                if cmp_sum < sum_match:
+                    # print(f'\t{x_l} -> {x_r}')
+                    sum_match = cmp_sum
+                    x_match = x_r
 
             # input('waiting...')
 
-            disp_map[unpadding_y, unpadding_x] = (x - match_x) or 255
+            # print(f'\t{x_l} -> {x_match}')
+            disp_map[y, x_l] = (x_l - x_match) or 255
+            matched[y, x_match] = 1
             # print()
             # print(f'disp[{unpadding_y}, {unpadding_x}]: {disp_map[unpadding_y, unpadding_x]}\n')
 
         # break
-        # if unpadding_y >= 150:
+        # if y >= 32:
         #     break
 
     return disp_map
