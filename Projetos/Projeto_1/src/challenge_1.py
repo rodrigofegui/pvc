9c10642f4d8e11aeebbfe870ad145aa760e8b60f
Rodrigo Ferrerira Guimarães <rodrigofegui@gmail.com>
2021-03-25 20:52:04 -0300

    Edit: Refatoraçao e reestruturaçao, separando medodos de mapa de disparidade

--- Projetos/Projeto_1/src/challenge_1.py
+++ Projetos/Projeto_1/src/challenge_1.py
@@ -1,9 +1,10 @@
-import cv2 as cv
 from datetime import datetime
-import numpy as np
 
-from utils.utils import get_disp_map, get_resize_shape, parse_calib_file
-from utils.variables import WORKDIRS, DISP_BLOCK_SEARCH, RESULT_DIR
+import cv2 as cv
+import numpy as np
+from utils.disparity_map import basic_disp_map, linear_search_disp_map, windowing_disp_map, x_correlation_disp_map
+from utils.utils import get_resize_shape, parse_calib_file
+from utils.variables import RESULT_DIR, WORKDIRS
 
 print('Challenge 1: Disparities and deepness\n')
 
@@ -21,14 +22,27 @@ for workdir in WORKDIRS:
     print('img_right', img_right.shape)
     print()
 
-    # disp_map = get_disp_map(img_left, img_right, version='default')
-    # disp_map = get_disp_map(img_left, img_right, version='ajust_window')
-    disp_map = get_disp_map(img_left, img_right, version='x_correlation')
-    # disp_map = get_disp_map(img_left, img_right, version='linear_search')
-    disp_max = np.max(disp_map)
-    cv.imwrite(f'{RESULT_DIR}/disp_max_{disp_max}_{datetime.now().isoformat()}.png', disp_map)
-    # disp_map = ((disp_map / disp_max) * 255).astype(np.float32)
-    # cv.imwrite(f'{RESULT_DIR}/disp_maxn_{disp_max}_{datetime.now().isoformat()}.png', disp_map)
+    gray_left = cv.cvtColor(img_left, cv.COLOR_BGR2GRAY)
+    gray_right = cv.cvtColor(img_right, cv.COLOR_BGR2GRAY)
+
+    methods = [
+        basic_disp_map,
+        linear_search_disp_map,
+        windowing_disp_map,
+        x_correlation_disp_map,
+    ]
+
+    for method in methods:
+        for block_sz in range(5, 15):
+            print(f'method: {method.__name__} | block_sz: {block_sz}')
+            disp_map = method(gray_left, gray_right, block_sz)
+
+            cv.imwrite(f'{RESULT_DIR}/{method.__name__}_bl{block_sz}.png', disp_map)
+
+            disp_max = np.max(disp_map)
+            disp_map = np.round(((disp_map / disp_max) * 255).astype(np.float32), decimals=0).astype(np.int32)
+
+            cv.imwrite(f'{RESULT_DIR}/{method.__name__}_bl{block_sz}_norm.png', disp_map)
 
 print('\nEnding: Challenge 1')
 print('-' * 50)
--- Projetos/Projeto_1/src/utils/utils.py
+++ Projetos/Projeto_1/src/utils/utils.py
@@ -1,10 +1,4 @@
-from os import cpu_count
-from threading import Thread
-
-import cv2 as cv
 import numpy as np
-
-from .variables import DISP_BLOCK_SEARCH, DISP_FILTER_RATIO, IMG_LOWEST_DIMENSION, MIN_DISP_FILTER_SZ
 
 
 def rolling_window(a, size):
@@ -14,7 +8,7 @@ def rolling_window(a, size):
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
 # https://colab.research.google.com/drive/1Zru_-zzbtylgitbwxbi0eDBNhwr8qYl6#scrollTo=R7GoWaxG2RpX
-def roll_2D(a,      # ND array
+def rolling_window_2D(a,      # ND array
          b,      # rolling 2D window array
          dx=1,   # horizontal step, abscissa, number of columns
          dy=1):  # vertical step, ordinate, number of rows
@@ -32,7 +26,6 @@ def closest_idx(array, value):
     array = np.asarray(array)
 
     return np.argmin(np.abs(array - value), axis=None)
-
 
 def parse_calib_file(file_name):
     with open(file_name, encoding='UTF-8') as file:
@@ -55,6 +48,8 @@ def parse_calib_file(file_name):
     }
 
 def get_resize_shape(original_shape: tuple) -> tuple:
+    from .variables import IMG_LOWEST_DIMENSION
+
     highest = 'width' if original_shape[0] < original_shape[1] else 'height'
 
     if highest == 'width':
@@ -62,108 +57,8 @@ def get_resize_shape(original_shape: tuple) -> tuple:
 
     return IMG_LOWEST_DIMENSION, int((IMG_LOWEST_DIMENSION * original_shape[0]) / original_shape[1])
 
-# def get_disp_map(img_left:np.ndarray, img_right:np.ndarray, use_default: bool = False) -> np.ndarray:
-def get_disp_map(img_left:np.ndarray, img_right:np.ndarray, version:str = 'ajust_window') -> np.ndarray:
-    """version = 'default' | 'ajust_window' """
-    valid_versions = ['default', 'ajust_window', 'x_correlation', 'linear_search']
-
-    if version not in valid_versions:
-        return np.zeros((*img_left.shape[:-1], 1))
-
-    img_left = cv.cvtColor(img_left, cv.COLOR_BGR2GRAY)
-    img_right = cv.cvtColor(img_right, cv.COLOR_BGR2GRAY)
-
-    filter_sz = int(max(MIN_DISP_FILTER_SZ, min(*img_left.shape) / DISP_FILTER_RATIO))
-    print(f'filter: {filter_sz}')
-
-    return eval(f'_get_disp_map_{version}')(img_left, img_right, filter_sz)
-
 def _parse_intrinsic(raw_line):
     return [
         list(map(float, row.strip().split()))
         for row in raw_line.replace('[', '').replace(']', '').split(';')
     ]
-
-def _get_disp_map_default(img_left:np.ndarray, img_right:np.ndarray, block_sz:int = MIN_DISP_FILTER_SZ) -> np.ndarray:
-    return cv.StereoBM_create(
-        # numDisparities=img_left.shape[1] - (img_left.shape[1] % 16),
-        numDisparities=DISP_BLOCK_SEARCH,
-        blockSize=block_sz
-    ).compute(img_left, img_right)
-
-def _get_disp_map_ajust_window(img_left:np.ndarray, img_right:np.ndarray, block_sz:int = MIN_DISP_FILTER_SZ) -> np.ndarray:
-    disp_map = np.zeros_like(img_left)
-    block_sz = int(block_sz / 2)
-    # matched = np.zeros((y_range[1], img_left.shape[1]))
-
-    for y in range(img_left.shape[0]):
-        print(f'linha: {y}')
-        for x_l in range(img_left.shape[1] - 1, -1, -1):
-            y_min, y_max = max(0, y - block_sz), min(y + block_sz + 1, img_left.shape[0])
-            x_min, x_max = max(0, x_l - block_sz), min(x_l + block_sz + 1, img_left.shape[1])
-
-            x_match = x_l
-
-            ref = img_left[y_min:y_max, x_min:x_max]
-
-            try:
-                search = roll_2D(img_right[y:y + ref.shape[0], :x_max], ref)
-
-                # print(f'\tsearch: {search.size}')
-                if search.size:
-                    search = search[0] - ref
-                    x_match = int(closest_idx(search, 0) / ref.size)
-            except Exception as e:
-                print('error', e)
-            finally:
-                disp_map[y, x_l] = (x_l - x_match) or 255
-        # if y >= 150:
-        #     break
-
-    return disp_map
-
-def _get_disp_map_x_correlation(img_left:np.ndarray, img_right:np.ndarray, block_sz:int = MIN_DISP_FILTER_SZ) -> np.ndarray:
-    disp_map = np.zeros_like(img_left)
-
-    padding = int(block_sz / 2)
-
-    img_left = np.pad(
-        img_left, [(padding, padding), (padding, padding)], mode='constant', constant_values=0
-    ).astype(np.int32)
-    img_right = np.pad(
-        img_right, [(padding, padding), (padding, padding)], mode='constant', constant_values=0
-    ).astype(np.int32)
-
-    for unpadded_y, y in enumerate(range(padding, img_left.shape[0] - padding)):
-        print(f'linha: {unpadded_y}')
-
-        for unpadded_x, x_l in enumerate(range(padding, img_left.shape[1] - padding - 1)):
-            ref = img_left[y, x_l:min(x_l + block_sz if x_l > block_sz else x_l + 1, img_left.shape[1])]
-
-            search = rolling_window(img_right[y, :x_l or 1], ref.size)
-            x_correlation = np.dot(search, ref) / (np.linalg.norm(search, axis=1) * np.linalg.norm(ref))
-
-            x_match = x_correlation.argmax()
-
-            disp_map[unpadded_y, unpadded_x] = x_l - x_match
-
-    return disp_map
-
-def _get_disp_map_linear_search(img_left:np.ndarray, img_right:np.ndarray, block_sz:int = MIN_DISP_FILTER_SZ) -> np.ndarray:
-    disp_map = np.zeros_like(img_left)
-
-    for y in range(img_left.shape[0]):
-        print(f'linha: {y}')
-        for x in range(img_left.shape[1]):
-            ref = img_left[y, x:min(x + block_sz if x > block_sz else x + 1, img_left.shape[1])]
-            search = img_right[y, :x or 1]
-            # print(f'ref: {ref.shape}\n{ref}')
-            # print(f'search: {search.shape}\n{search}')
-
-            search = rolling_window(search, ref.size) - ref
-
-            x_match = int(closest_idx(search, 0) / ref.size)
-
-            disp_map[y,x] = (x - x_match) or 255
-
-    return disp_map
